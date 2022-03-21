3636875696010304f33caf1042f9a898e64eaf79
ayoubassairh <ayoub.assairh@gmail.com>
2022-03-21 22:57:39 +0100

    Add block for heart and price, scss done and function done

--- scripts/utils/displayBlockHeartAndPrice.js
+++ scripts/utils/displayBlockHeartAndPrice.js
@@ -1,1 +1,24 @@
-export const displayBlockHeartAndPrice = () => {};
+import { createElementToCard } from './functions.js';
+
+export const displayBlockHeartAndPrice = (mediaData, photographer) => {
+    const likesSum = mediaData
+        .map((media) => media.likes)
+        .reduce((prev, curr) => prev + curr, 0);
+
+    const elementDiv = document.getElementById('block-heart');
+
+    const elementSpanHeart = createElementToCard('span', likesSum, null);
+    const elementIcon = createElementToCard('i', null, [
+        { attribut: 'class', content: 'fa fa-heart' },
+    ]);
+    elementSpanHeart.appendChild(elementIcon);
+
+    const elementSpanPrice = createElementToCard(
+        'span',
+        `${photographer.price} €/jours `,
+        null
+    );
+
+    elementDiv.appendChild(elementSpanHeart);
+    elementDiv.appendChild(elementSpanPrice);
+};
--- scripts/utils/displayPhotographerPage.js
+++ scripts/utils/displayPhotographerPage.js
@@ -1,6 +1,7 @@
 import { displaySelect } from './displaySelect.js';
 import { displayPhotographerHeader } from '../pages/photographer.js';
 import { getListOfMediaByID, selectControl } from './functions.js';
+import { displayBlockHeartAndPrice } from './displayBlockHeartAndPrice.js';
 
 /**
  * DISPLAY Photographer page with his header and his media
@@ -12,12 +13,13 @@ export const displayPhotographerPage = (data, idFromUrl) => {
         (photographer) => photographer.id == idFromUrl
     );
     displayPhotographerHeader(photographer);
-    displaySelect();
     const mediasData = data.media.filter(
         (media) => media.photographerId == idFromUrl
     );
+    displaySelect(mediasData);
 
     const listOfMediaById = getListOfMediaByID(mediasData, photographer);
+    displayBlockHeartAndPrice(mediasData, photographer);
 
     let mediaSection = document.getElementById('medias');
 
